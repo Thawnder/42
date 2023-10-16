7686bc72d36159a00a42404894318ba4e1801f07
Benjamin Pleutin <bpleutin@c2r8p2.42perpignan.fr>
2023-10-16 13:20:04 +0200

    debut philo

---
+++ Philo/philo_bonus/philo.h
@@ -0,0 +1,17 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <semaphore.h>
+# include <sys/stat.h>
+# include <signal.h>
+
+
+
+# endif
