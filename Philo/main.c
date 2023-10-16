e92d46a6600f8113e71951ea612bad53353fb437
Benjamin Pleutin <bpleutin@c2r8p2.42perpignan.fr>
2023-10-16 16:56:49 +0200

    bon

--- Philo/main.c
+++ Philo/main.c
@@ -6,19 +6,91 @@
 /*   By: bpleutin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 11:06:13 by bpleutin          #+#    #+#             */
-/*   Updated: 2023/10/16 11:13:08 by bpleutin         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:44:10 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <philo.h>
+#include "philo.h"
+
+alltime	ft_atoll(char *nptr)
+{
+	int				i;
+	int				sign;
+	alltime			res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + nptr[i] - 48;
+		i++;
+	}
+	return ((int)res * sign);
+}
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	res;
+	size_t	i;
+
+	res = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if (res / size != nmemb || res / nmemb != size)
+		return (NULL);
+	ptr = malloc(res);
+	i = 0;
+	if (ptr)
+	{
+		while (i < res)
+		{
+			*(unsigned char *)(ptr + i) = 0;
+			i++;
+		}
+	}
+	return (ptr);
+}
+
+void	init_all(t_data *data, int argc, char **argv)
+{
+	data->gang_size = (int) ft_atoll(argv[1]);
+	data->time_to_die = ft_atoll(argv[2]);
+	data->time_to_eat = ft_atoll(argv[3]);
+	data->time_to_sleep = ft_atoll(argv[4]);
+	if (argc == 6)
+		data->nbr_of_meals = (int) ft_atoll(argv[5]);
+	data->philo = ft_calloc(data->gang_size, sizeof(t_philo));
+}
+
+void	free_all(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->philo[i])
+		free(data->philo[i++]);
+}
 
 int	main(int argc, char **argv)
 {
+	t_data	data;
+
 	if (argc < 5 || argc > 6)
 	{
 		printf("Invalid number of args\n");
 		return (-1);
 	}
-
+	init_all(&data, argc, argv);
+	free_all(&data);
+	return (0);
 }
 
--- Philo/philo.h
+++ Philo/philo.h
@@ -3,10 +3,32 @@
 
 # include <string.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
 
+typedef unsigned long long int alltime;
 
+typedef struct s_philo
+{
+	int		id;
+	pthread_t	tid;
+	alltime		countdown;
+	int		state;
+	pthread_mutex_t	*left;
+	pthread_mutex_t	*right;
+}		t_philo;
+
+typedef struct s_data
+{
+	int	gang_size;
+	int	nbr_of_meals;
+	alltime	time_to_die;
+	alltime	time_to_eat;
+	alltime	time_to_sleep;
+	t_philo	*philo;
+
+}		t_data;
 
 #endif
